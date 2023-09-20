3ca90af6dadf987d1445b5d33dbb09196f9a639b
Habtetsega <kido3871@gmail.com>
2023-09-20 10:27:44 -0700

    Update the script to replace the value 12 with 89

--- javascript-warm_up/0-javascript_is_amazing.js
+++ javascript-warm_up/0-javascript_is_amazing.js
@@ -1,5 +1,2 @@
-int main(){
-	const myVar;
-	console.log("JavaScript is amazing");
-
-}
+const myVar;
+console.log("JavaScript is amazing");
--- javascript-warm_up/1-multi_languages.js
+++ javascript-warm_up/1-multi_languages.js
@@ -1,5 +1,3 @@
-int main(){
-	console.log("C is fun");
-	console.log("Python is cool");
-	console.log("JavaScript is amazing");
-}
+console.log("C is fun");
+console.log("Python is cool");
+console.log("JavaScript is amazing");
