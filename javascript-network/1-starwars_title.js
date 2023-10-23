b2005a881e5151ffc71f97a0a888aaebc98aa1fc
Habtetsega <kido3871@gmail.com>
2023-10-23 07:33:19 -0700

    star war

--- javascript-network/1-starwars_title.js
+++ javascript-network/1-starwars_title.js
@@ -1,11 +1,24 @@
 #!/usr/bin/node
-const r = require('request');
-const url = "https://swapi-api.alx-tools.com/api/films/:id";
-r(url, (error, response, body)=>{
-	if (error){
-		console.error('Error:', error);
-	}else{
-		const movie = JSON.parse(body);
-		console.log(movie.title);
-	}
+const request = require('request');
+
+// Get the movie ID from command line arguments
+const movieId = process.argv[2];
+
+// Construct the API endpoint URL
+const url = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
+
+// Send a GET request to the API endpoint
+request.get(url, (error, response, body) => {
+  if (error) {
+    console.error('Error:', error);
+  } else {
+    // Parse the response body as JSON
+    const data = JSON.parse(body);
+
+    // Extract the movie title from the response
+    const movieTitle = data.title;
+
+    // Print the movie title
+    console.log(movieTitle);
+  }
 });
